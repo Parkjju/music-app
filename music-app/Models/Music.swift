fe70b37b37e002b1327c91b044c8f661483070a5
Parkjju <rudwns3927@gmail.com>
2023-01-31 08:37:21 +0900

    feat: releaseDate 날짜 포맷팅 및 구조체 계산속성 추가

--- music-app/Models/Music.swift
+++ music-app/Models/Music.swift
@@ -22,6 +22,17 @@ struct MusicResult: Codable{
     var collectionViewUrl: String?
     var trackViewUrl: String?
     var previewUrl: String?
-    var releaseDate: String?
     var primaryGenreName: String?
+    var releaseDate:String?
+    
+    var releaseDateString: String? {
+        guard let isoDate = ISO8601DateFormatter().date(from: releaseDate ?? "") else {
+            return ""
+        }
+        
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "yyyy-mm-dd"
+        let dateString = myFormatter.string(from: isoDate)
+        return dateString
+    }
 }
